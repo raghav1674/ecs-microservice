a7c4ec40fa7158c07a770fc92ce8c4434aaebe0f
Raghav Gupta <ragaws1674@gmail.com>
2022-05-14 15:32:47 +0530

    Add call to notify microservice from user microservice

--- usermanagement/index.js
+++ usermanagement/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const axios = require("axios");
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize(process.env.AWS_RDS_DB, process.env.AWS_RDS_USERNAME, process.env.AWS_RDS_PASSWORD, {
     host: process.env.AWS_RDS_HOST,
     dialect: 'postgres'
 });
+const NOTIFY_SERVICE_ENDPOINT = process.env.NOTIFY_SERVICE_ENDPOINT;
 const User = sequelize.define('User', {
     // Model attributes are defined here
     firstName: {
@@ -26,11 +28,11 @@ const User = sequelize.define('User', {
 const app = express();
 app.use(express.json())
 
-app.use("/",(req,res,next)=>{
+app.use("/", (req, res, next) => {
 
-    res.setHeader("Access-Control-Allow-Origin","*")
-    res.setHeader("Access-Control-Allow-Methods","GET,POST,OPTIONS")
-    res.setHeader("Access-Control-Allow-Headers","*")
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+    res.setHeader("Access-Control-Allow-Headers", "*")
     next();
 })
 
@@ -46,7 +48,8 @@ app.post('/register', async (req, res) => {
     try {
         const user = await User.create({ firstName, lastName, email });
         if (user) {
-
+            const response = await __call_to_notify_service(firstName,lastName);
+            console.log(response.data);
             return res.status(201).json({ message: 'User created' })
         }
         return res.status(400).json({ error: 'Bad Request' })
@@ -57,6 +60,27 @@ app.post('/register', async (req, res) => {
     }
 
 })
+
+async function __call_to_notify_service(firstName, lastName) {
+
+    try {
+        const response = await axios({
+            method: 'post',
+            url: NOTIFY_SERVICE_ENDPOINT,
+            data: {
+                firstName,
+                lastName
+            }
+        });
+        return response;
+
+    } catch (error) {
+        console.log(error.message);
+        return {"error": error.message}
+    }
+
+
+}
 
 
 const port = process.env.PORT || 4000;
